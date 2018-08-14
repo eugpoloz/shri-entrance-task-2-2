21ca21e7c00f348d30f20212aca9de2ef5e380b9
Evgenia Polozova <eugpoloz@gmail.com>
2018-08-14 23:26:50 +0300

    update gulpfile and package.json to use browser-sync, rename some dirs make dependencies straight

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-var gulp = require('gulp'),
-  pug = require('gulp-pug'),
-  autoprefixer = require('gulp-autoprefixer'),
-  stylus = require('gulp-stylus'),
-  connect = require('gulp-connect'),
-  log = require('fancy-log'),
-  imagemin = require('gulp-imagemin');
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var autoprefixer = require('gulp-autoprefixer');
+var stylus = require('gulp-stylus');
+var connect = require('gulp-connect');
+var log = require('fancy-log');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
 
 //Log
 gulp.task('log', function() {
@@ -14,7 +16,7 @@ gulp.task('log', function() {
 //Stylus & Autoprefixer Comp
 gulp.task('stylus', function() {
   gulp
-    .src('app/*.styl')
+    .src('src/*.styl')
     .pipe(stylus({ 'include css': true }))
     .on('error', log)
     .pipe(
@@ -22,51 +24,61 @@ gulp.task('stylus', function() {
     )
     .on('error', log)
     .pipe(gulp.dest('dist'))
-    .pipe(connect.reload());
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 //Pug Comp
 gulp.task('pug', function() {
   gulp
-    .src('app/index.pug')
+    .src('src/index.pug')
     .pipe(pug({ pretty: true }))
     .on('error', log)
     .pipe(gulp.dest('dist'))
-    .pipe(connect.reload());
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 //JS Comp
 gulp.task('js', function() {
   gulp
-    .src('app/*.js')
+    .src('src/*.js')
     .pipe(gulp.dest('dist'))
-    .pipe(connect.reload());
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 //Copy assets
-gulp.task('copy', function() {
+gulp.task('assets', function() {
   gulp
-    .src('app/images/*.*')
+    .src('src/assets/*.*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('dist/assets'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 //Watch for changes
 gulp.task('watch', function() {
-  gulp.watch('app/*.styl', ['stylus']);
-  gulp.watch('app/index.pug', ['pug']);
-  gulp.watch('app/*.js', ['js']);
-  gulp.watch('app/images/*.*', ['copy']);
+  gulp.watch('src/*.styl', ['stylus']);
+  gulp.watch('src/index.pug', ['pug']);
+  gulp.watch('src/*.js', ['js']);
+  gulp.watch('src/assets/*.*', ['copy']);
 });
 
-//Set up the server for life reload
-gulp.task('connect', function() {
-  connect.server({
-    root: '.',
-    livereload: true
+// Set up the static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+      index: 'index.html'
+    },
+    port: 8080
   });
 });
 
 //Setting up the starter gulp package
-gulp.task('default', ['stylus', 'pug', 'js', 'copy', 'connect', 'watch']);
+gulp.task('default', [
+  'stylus',
+  'pug',
+  'js',
+  'assets',
+  'browser-sync',
+  'watch'
+]);
